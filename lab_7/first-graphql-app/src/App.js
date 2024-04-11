c66bd7b6d108477cad1ee233950f55ff6ae8ab8f
Daarkosss <karolpilat59@gmail.com>
2024-04-11 16:05:53 +0200

    Fetch real data from REST service for todos

--- lab_7/first-graphql-app/src/App.js
+++ lab_7/first-graphql-app/src/App.js
@@ -8,21 +8,6 @@ import axios from "axios";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const typeDefs = readFileSync(join(__dirname, 'schema.graphql'), 'utf8');
 
-const usersList = [     
-  { id: 1, name: "Jan Konieczny", email: "jan.konieczny@wonet.pl", login: "jkonieczny" },
-  { id: 2, name: "Anna Wesołowska", email: "anna.w@sad.gov.pl", login: "anna.wesolowska" },     
-  { id: 3, name: "Piotr Waleczny", email: "piotr.waleczny@gp.pl", login: "p.waleczny" } 
-];  
-
-const todosList = [     
-  { id: 1, title: "Naprawić samochód", completed: false, user_id: 3 },     
-  { id: 2, title: "Posprzątać garaż",  completed: true,  user_id: 3 },     
-  { id: 3, title: "Napisać e-mail",    completed: false, user_id: 3 },     
-  { id: 4, title: "Odebrać buty",      completed: false, user_id: 2 },     
-  { id: 5, title: "Wysłać paczkę",     completed: true,  user_id: 2 },     
-  { id: 6, title: "Zamówic kuriera",   completed: false, user_id: 3 }, 
-];
-
 function todoById(parent, args, context, info) {     
   return todosList.find(t => t.id == args.id); 
 }  
@@ -46,27 +31,45 @@ async function getRestUsersList() {
   }
 }
 
+async function getRestTodosList() {
+  try {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}
+
 
 const yoga = createYoga({
   schema: createSchema({
     typeDefs: typeDefs,
     resolvers: {
       Query: {
-        users: async () => getRestUsersList(),         
-        todos: () => todosList,       
-        todo: (parent, args, context, info) => todoById(parent, args, context, info),
-        user: (parent, args, context, info) => userById(parent, args, context, info),
+        users: async () => await getRestUsersList(),         
+        todos: async () => await getRestTodosList(),       
+        todo: async (parent, { id }) => {
+          const todos = await getRestTodosList();
+          return todos.find(todo => todo.id === Number(id));
+        },
+        user: async (parent, { id }) => {
+          const users = await getRestUsersList();
+          return users.find(user => user.id === Number(id));
+        },
       },
       User: {         
-        todos: (parent, args, context, info) => {             
-          return todosList.filter(t => t.user_id == parent.id);         
+        todos: async (parent) => {
+          const todos = await getRestTodosList();
+          return todos.filter(todo => todo.userId === parent.id);
         }      
       },
       ToDoItem: {         
-        user: (parent, args, context, info) => {             
-          return usersList.find(u => u.id == parent.user_id);         
+        user: async (parent) => {
+          const users = await getRestUsersList();
+          return users.find(user => user.id === parent.userId);
         }     
-      }, 
+      },
     }
   })
 })
